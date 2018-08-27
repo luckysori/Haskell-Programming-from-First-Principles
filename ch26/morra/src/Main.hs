fc0a38f8578bf8070fff850c252f588cfb86d685
Lucas Soriano del Pino <l.soriano.del.pino@gmail.com>
2018-08-27 11:52:31 +1000

    morra game only lacking computer trigram-based ai

--- ch26/morra/src/Main.hs
+++ ch26/morra/src/Main.hs
@@ -1,6 +1,5 @@
 module Main where
 
-import           Control.Monad             (join)
 import           Control.Monad.Trans.Class
 import           Control.Monad.Trans.State
 import           System.Console.ANSI
@@ -16,27 +15,30 @@ instance Show Player where
 data PlayerType = Human | CPU
 type Mode = (PlayerType, PlayerType)
 
+type Parity = Int -> Bool
+
 data GameState = GameState
               { mode         :: Mode
+              , p1WinsOn     :: Parity
               , maxRoundWins :: Int
               , gameScore    :: (Int, Int)
               , roundScore   :: (Int, Int)}
 
 checkRoundWinner :: StateT GameState IO Player
 checkRoundWinner = do
-  (GameState _ _ (gs1, gs2) (rs1, rs2)) <- get
-  let rw = if even $ rs1 + rs2 then P1 else P2
+  (GameState _ f _ (gs1, gs2) (rs1, rs2)) <- get
+  let rw = if f $ rs1 + rs2 then P1 else P2
   lift $ putStrLn (show rw ++ " wins round number " ++ show (gs1 + gs2 + 1) ++ ".")
   return rw
 
 updateGameScore :: Player -> StateT GameState IO ()
 updateGameScore p = -- p is the player that won the most recent round
-  modify (\(GameState m o (s1, s2) rs) -> do
+  modify (\(GameState m f o (s1, s2) rs) -> do
              let ns = if p == P1 then (s1 + 1, s2) else (s1, s2 + 1)
-             GameState m o ns rs)
+             GameState m f o ns rs)
 
 checkGameWinner :: StateT GameState IO (Maybe Player)
-checkGameWinner = gets (\(GameState _ o (s1, s2) _)
+checkGameWinner = gets (\(GameState _ _ o (s1, s2) _)
                         -> if s1 == o then Just P1 else if s2 == o then Just P2 else Nothing)
 
 getNumber :: Int -> StateT GameState IO (Maybe Int)
@@ -44,7 +46,7 @@ getNumber n
   | n < 0 || n > 1 = return Nothing -- should never go here
   | otherwise = do
       let fs = [fst, snd]
-      (GameState m _ _ _) <- get
+      (GameState m _ _ _ _) <- get
       case fs !! n $ m of
         Human -> do
           lift $ putStrLn $
@@ -61,19 +63,25 @@ morra = do
   (Just p2N) <- getNumber 1
   lift $ putStrLn $ "Player 1 chose: " ++ show p1N
   lift $ putStrLn $ "Player 2 chose: " ++ show p2N
-  modify (\(GameState m o gs _) -> GameState m o gs (p1N, p2N))
+  modify (\(GameState m f o gs _) -> GameState m f o gs (p1N, p2N))
   rw <- checkRoundWinner
   updateGameScore rw
+  (GameState _ _ _ (gs1, gs2) _) <- get
+  lift $ putStrLn $ "The score is " ++ show gs1 ++ "-" ++ show gs2
   gw <- checkGameWinner
   case gw of
     Just p  -> return p
     Nothing -> morra
 
+selParFunc :: Int -> Parity
+selParFunc n = if even n then even else odd
+
 readConfig :: [String] -> GameState
-readConfig ["2", n] = GameState (Human, Human) (read n) (0,0) (0,0)
-readConfig ["1", n] = GameState (Human, CPU) (read n) (0,0) (0,0)
-readConfig ["0", n] = GameState (CPU, CPU) (read n) (0,0) (0,0)
-readConfig _        = GameState (Human, CPU) 2 (0,0) (0,0)
+readConfig ["2", p, rw] = GameState (Human, Human) (selParFunc $ read p) (read rw) (0,0) (0,0)
+readConfig ["1", p, rw] = GameState (Human, CPU) (selParFunc $ read p) (read rw) (0,0) (0,0)
+readConfig ["0", p, rw] = GameState (CPU, CPU) (selParFunc $ read p) (read rw) (0,0) (0,0)
+readConfig _            = GameState (Human, CPU) even 2 (0,0) (0,0)
+-- default: human vs cpu, human wins rounds on even sums, game won on first to 2 rounds
 
 main :: IO ()
 main = do
